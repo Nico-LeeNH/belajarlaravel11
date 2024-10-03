15d84672e6ea0973289dee1cc55f7665aa7b3055
Nico Lee <new.nikolee2007@gmail.com>
2024-10-03 18:08:53 +0700

    style: add component

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['title' => 'Home Page']);
 });
 Route::get('/about', function () {
-    return view('about', ['nama' => 'nico']);
+    return view('about', ['title' => 'about']);
 });
 Route::get('/blog', function () {
-    return view('blog');
+    return view('blog', ['title' => 'Blog']);
 });
 Route::get('/contact', function () {
     return view('contact');
