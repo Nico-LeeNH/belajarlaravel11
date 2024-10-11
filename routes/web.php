d10849a595555e7606403790d798533eacda2d59
Nico Lee <new.nikolee2007@gmail.com>
2024-10-11 15:56:04 +0700

    style: add singel post page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+
+use function Pest\Laravel\post;
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
@@ -11,16 +14,43 @@ Route::get('/about', function () {
 Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog', 'posts' =>[
         [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
             'title' => 'artikel 1',
             'author' => 'unknown',
             'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero doloremque, iusto consequuntur accusamus obcaecati minus est, reprehenderit excepturi debitis iure, illo nisi! Dolore soluta earum, maiores velit perspiciatis veniam. Nihil.'
         ],
         [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
             'title' => 'artikel 1',
             'author' => 'unknown',
             'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero doloremque, iusto consequuntur accusamus obcaecati minus est, reprehenderit excepturi debitis iure, illo nisi! Dolore soluta earum, maiores velit perspiciatis veniam. Nihil.'
         ]
     ]]);
+});
+
+Route::get('/post/{slug}', function ($slug){
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'artikel 1',
+            'author' => 'unknown',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero doloremque, iusto consequuntur accusamus obcaecati minus est, reprehenderit excepturi debitis iure, illo nisi! Dolore soluta earum, maiores velit perspiciatis veniam. Nihil.'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'artikel 1',
+            'author' => 'unknown',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero doloremque, iusto consequuntur accusamus obcaecati minus est, reprehenderit excepturi debitis iure, illo nisi! Dolore soluta earum, maiores velit perspiciatis veniam. Nihil.'
+        ],
+    ];
+    $post = Arr::first($posts, function ($post) use ($slug){
+        return $post['slug'] == $slug;
+    });
+    return view('post', ['title' => 'single post', 'post' => $post]);
 });
 Route::get('/contact', function () {
     return view('contact', ['title' => 'contact']);
