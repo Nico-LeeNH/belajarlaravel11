33c5c12c0d99300426406da3c53a16dde52145c8
Nico Lee <new.nikolee2007@gmail.com>
2024-10-14 18:57:39 +0700

    add model factories

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -10,7 +10,7 @@
             <a href="">{{ $post ['author'] }} | {{ $post->created_at->diffForHumans() }}</a>
         </div>
         <p class="my-4 font-light">
-            {{Str::limit ($post ['body'], 100) }}
+            {{Str::limit ($post ['body'], 200) }}
         </p>
         <a href="/post/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
     </article>
